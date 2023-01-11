7a98f3cb61ea6473dd737e14cafbbfe62e7a36e2
mejiaj <jesse.mejia@saintandrews.net>
2023-01-11 11:56:40 -0500

    first push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         HuffmanAlgorithm huffman = new HuffmanAlgorithm();
-        huffman.buildFrequencyTable("It was her first experience training a rainbow unicorn.");
+        huffman.buildFrequencyTable("It was her first experience training a rainbow unicorn. ");
     }
 }
